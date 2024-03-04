d25f7ada42756832e5adfa6b0c8b41356cd04224
aDisplayName <mattali@gmail.com>
2024-03-04 09:10:01 -0600

    Add code example for issue https://github.com/dotnet/runtime/issues/97475

---
+++ 20240124/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+var builder= Host.CreateEmptyApplicationBuilder(new HostApplicationBuilderSettings());
+builder.Services
+    .AddHostedService<HostClass>()
+    .AddSingleton<IMethod>(sp => sp.GetRequiredService<INewMethod>())
+    .AddSingleton<INewMethod, MethodClass>();
+        
+var host = builder.Build();
+await host.RunAsync();
+
+
+internal interface IMethod {}
+
+internal interface INewMethod: IMethod {}
+
+internal class MethodClass : INewMethod, IAsyncDisposable
+{
+    private readonly SemaphoreSlim _lock = new(1, 1);
+    public async ValueTask DisposeAsync()
+    {   
+        await _lock.WaitAsync();
+        _lock.Release();
+        _lock.Dispose();
+    }
+}
+
+
+class HostClass(
+    IMethod method,
+    IHostApplicationLifetime appLifetime) : IHostedService
+{
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        appLifetime.StopApplication();
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
+
